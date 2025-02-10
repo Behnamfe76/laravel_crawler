1a0c9b5114cb3a3847ea63c9244038e107d7ee73
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-02-10 09:58:59 +0330

    feat(seeders): new seeders created for users and selenium drivers

---
+++ database/seeders/SeleniumDriverSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\SeleniumDriver;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SeleniumDriverSeeder extends Seeder
+{
+    private array $seleniumDrivers = [
+        [
+            'name' => 'local driver',
+            'host' => '127.0.0.1',
+            'port' => 4444,
+        ],[
+            'name' => 'docker node 1',
+            'host' => '127.0.0.1',
+            'port' => 4445,
+        ], [
+            'name' => 'docker node 2',
+            'host' => '127.0.0.1',
+            'port' => 4446,
+        ],
+    ];
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        foreach ($this->seleniumDrivers as $seleniumDriver) {
+            SeleniumDriver::create($seleniumDriver);
+        }
+    }
+}
